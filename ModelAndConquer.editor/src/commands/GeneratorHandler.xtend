877456060bc2c25c5ce5cbedb548a673eade10ce
olli <oliver.schlalos@gmx.de>
2025-07-01 13:43:16 +0200

    feat: add generator boilerplate code

--- ModelAndConquer.editor/src/commands/GeneratorHandler.xtend
+++ ModelAndConquer.editor/src/commands/GeneratorHandler.xtend
@@ -3,11 +3,36 @@ package commands
 import org.eclipse.core.commands.AbstractHandler
 import org.eclipse.core.commands.ExecutionEvent
 import org.eclipse.core.commands.ExecutionException
+import org.eclipse.ui.IWorkbenchPart
+import org.eclipse.ui.handlers.HandlerUtil
+import ModelAndConquer.presentation.ModelAndConquerEditor
+import org.eclipse.core.resources.IProject
+import org.eclipse.ui.IFileEditorInput
+import org.eclipse.jface.viewers.ISelection
+import org.eclipse.jface.viewers.TreeSelection
 
 class GeneratorHandler extends AbstractHandler {
 	
 	override execute(ExecutionEvent event) throws ExecutionException {
 		println("Generator starting...");
+		
+		// Get active part of the model eclipse instance
+		val IWorkbenchPart wbp = HandlerUtil.getActivePart(event);
+		
+		// Cast to project editor
+		var ModelAndConquerEditor ue = wbp as ModelAndConquerEditor;
+			
+		// Get active project in the model eclipse instance
+		var IProject project = (ue.getEditorInput() as IFileEditorInput).file.project;
+		
+		// Get selected element
+		var TreeSelection ts = ue.selection as TreeSelection;
+		
+		var root = ts.firstElement		
+		
+		return void
 	}
+	
+	
 	
 }
--- ModelAndConquer.editor/xtend-gen/commands/GeneratorHandler.java
+++ ModelAndConquer.editor/xtend-gen/commands/GeneratorHandler.java
@@ -1,14 +1,30 @@
 package commands;
 
+import ModelAndConquer.presentation.ModelAndConquerEditor;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class GeneratorHandler extends AbstractHandler {
   @Override
   public Object execute(final ExecutionEvent event) throws ExecutionException {
-    return InputOutput.<Object>println("Generator starting...");
+    InputOutput.<String>println("Generator starting...");
+    final IWorkbenchPart wbp = HandlerUtil.getActivePart(event);
+    ModelAndConquerEditor ue = ((ModelAndConquerEditor) wbp);
+    IEditorInput _editorInput = ue.getEditorInput();
+    IProject project = ((IFileEditorInput) _editorInput).getFile().getProject();
+    ISelection _selection = ue.getSelection();
+    TreeSelection ts = ((TreeSelection) _selection);
+    Object root = ts.getFirstElement();
+    return void.class;
   }
 }
