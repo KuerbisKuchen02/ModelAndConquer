13568e4d5bed192a6ddf5be8d1b5e90cc0cfea0a
DerHighman <jannikheimann3@gmail.com>
2025-07-08 17:42:08 +0200

    feat: add write to file method and dynamically generating of EDamageType class

--- ModelAndConquer.editor/src/commands/GeneratorHandler.xtend
+++ ModelAndConquer.editor/src/commands/GeneratorHandler.xtend
@@ -1,15 +1,21 @@
 package commands
 
+import ModelAndConquer.DamageType
+import ModelAndConquer.Game
+import ModelAndConquer.presentation.ModelAndConquerEditor
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 import org.eclipse.core.commands.AbstractHandler
 import org.eclipse.core.commands.ExecutionEvent
 import org.eclipse.core.commands.ExecutionException
+import org.eclipse.core.resources.IFile
+import org.eclipse.core.resources.IFolder
+import org.eclipse.core.resources.IProject
+import org.eclipse.emf.common.util.EList
+import org.eclipse.jface.viewers.TreeSelection
+import org.eclipse.ui.IFileEditorInput
 import org.eclipse.ui.IWorkbenchPart
 import org.eclipse.ui.handlers.HandlerUtil
-import ModelAndConquer.presentation.ModelAndConquerEditor
-import org.eclipse.core.resources.IProject
-import org.eclipse.ui.IFileEditorInput
-import org.eclipse.jface.viewers.ISelection
-import org.eclipse.jface.viewers.TreeSelection
 
 class GeneratorHandler extends AbstractHandler {
 	
@@ -28,11 +34,68 @@ class GeneratorHandler extends AbstractHandler {
 		// Get selected element
 		var TreeSelection ts = ue.selection as TreeSelection;
 		
-		var root = ts.firstElement		
+		var Game game = ts.firstElement as Game;		
+		
+		
+		// Generate all EDamageTypes
+		createFileWithContent(project, "models", "EDamageType.java", generateEDamageTypes(game.damageTypes));
 		
 		return void
 	}
 	
 	
+	def generateEDamageTypes(EList<DamageType> damageTypes) '''
+	public enum EDamageType {
+		«FOR int i: 0..damageTypes.size-1 SEPARATOR ','»
+		«damageTypes.get(i).name.toUpperCase»(«i»)«IF i==damageTypes.size-1»;«ENDIF»
+		«ENDFOR»
+		
+		private final int value;
+		
+		EDamageType(int value) { this.value=value; }
+		
+		public static String getValueString(int value) {
+			return switch (value) {
+				«FOR int i: 0..damageTypes.size-1»
+				case «i» -> «damageTypes.get(i).name»
+				«ENDFOR»
+				default -> "Not an valid DamageType!"
+			}
+		}
+	}
+	'''
 	
+	
+	def void createFileWithContent(IProject project, String pckgName, String fileName, CharSequence content) {
+		var String currentFolderString = "src-gen/";
+		var IFolder folder
+		
+		folder = project.getFolder("src-gen");
+		if(!folder.exists) {
+			folder.create(true, true, null)
+		}
+	
+		for(String n : pckgName.split("\\.")) { //de . thm . zdh . Toll
+			currentFolderString = currentFolderString + n + "/";
+			folder = project.getFolder(currentFolderString);
+			
+			if(!folder.exists) {
+				folder.create(true,true,null);
+			}
+		}
+
+		var IFile file = folder.getFile(fileName);
+	
+		if(file.exists) {
+			file.delete(true,null);
+		}
+	
+		if(!file.exists) {
+			var byte[] bytes;
+			bytes = content.toString.bytes;
+			var InputStream source = new ByteArrayInputStream(bytes);
+			file.create(source, true, null);
+		}
+	}
+
 }
--- ModelAndConquer.editor/xtend-gen/commands/GeneratorHandler.java
+++ ModelAndConquer.editor/xtend-gen/commands/GeneratorHandler.java
@@ -1,17 +1,27 @@
 package commands;
 
+import ModelAndConquer.DamageType;
+import ModelAndConquer.Game;
 import ModelAndConquer.presentation.ModelAndConquerEditor;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.eclipse.xtext.xbase.lib.IntegerRange;
 
 @SuppressWarnings("all")
 public class GeneratorHandler extends AbstractHandler {
@@ -24,7 +34,127 @@ public class GeneratorHandler extends AbstractHandler {
     IProject project = ((IFileEditorInput) _editorInput).getFile().getProject();
     ISelection _selection = ue.getSelection();
     TreeSelection ts = ((TreeSelection) _selection);
-    Object root = ts.getFirstElement();
+    Object _firstElement = ts.getFirstElement();
+    Game game = ((Game) _firstElement);
+    this.createFileWithContent(project, "models", "EDamageType.java", this.generateEDamageTypes(game.getDamageTypes()));
     return void.class;
+  }
+
+  public CharSequence generateEDamageTypes(final EList<DamageType> damageTypes) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("public enum EDamageType {");
+    _builder.newLine();
+    {
+      int _size = damageTypes.size();
+      int _minus = (_size - 1);
+      IntegerRange _upTo = new IntegerRange(0, _minus);
+      boolean _hasElements = false;
+      for(final int i : _upTo) {
+        if (!_hasElements) {
+          _hasElements = true;
+        } else {
+          _builder.appendImmediate(",", "\t");
+        }
+        _builder.append("\t");
+        String _upperCase = damageTypes.get(i).getName().toUpperCase();
+        _builder.append(_upperCase, "\t");
+        _builder.append("(");
+        _builder.append(i, "\t");
+        _builder.append(")");
+        {
+          int _size_1 = damageTypes.size();
+          int _minus_1 = (_size_1 - 1);
+          boolean _equals = (i == _minus_1);
+          if (_equals) {
+            _builder.append(";");
+          }
+        }
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("private final int value;");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("EDamageType(int value) { this.value=value; }");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("public static String getValueString(int value) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("return switch (value) {");
+    _builder.newLine();
+    {
+      int _size_2 = damageTypes.size();
+      int _minus_2 = (_size_2 - 1);
+      IntegerRange _upTo_1 = new IntegerRange(0, _minus_2);
+      for(final int i_1 : _upTo_1) {
+        _builder.append("\t\t\t");
+        _builder.append("case ");
+        _builder.append(i_1, "\t\t\t");
+        _builder.append(" -> ");
+        String _name = damageTypes.get(i_1).getName();
+        _builder.append(_name, "\t\t\t");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\t\t\t");
+    _builder.append("default -> \"Not an valid DamageType!\"");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+
+  public void createFileWithContent(final IProject project, final String pckgName, final String fileName, final CharSequence content) {
+    try {
+      String currentFolderString = "src-gen/";
+      IFolder folder = null;
+      folder = project.getFolder("src-gen");
+      boolean _exists = folder.exists();
+      boolean _not = (!_exists);
+      if (_not) {
+        folder.create(true, true, null);
+      }
+      String[] _split = pckgName.split("\\.");
+      for (final String n : _split) {
+        {
+          currentFolderString = ((currentFolderString + n) + "/");
+          folder = project.getFolder(currentFolderString);
+          boolean _exists_1 = folder.exists();
+          boolean _not_1 = (!_exists_1);
+          if (_not_1) {
+            folder.create(true, true, null);
+          }
+        }
+      }
+      IFile file = folder.getFile(fileName);
+      boolean _exists_1 = file.exists();
+      if (_exists_1) {
+        file.delete(true, null);
+      }
+      boolean _exists_2 = file.exists();
+      boolean _not_1 = (!_exists_2);
+      if (_not_1) {
+        byte[] bytes = null;
+        bytes = content.toString().getBytes();
+        InputStream source = new ByteArrayInputStream(bytes);
+        file.create(source, true, null);
+      }
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
   }
 }
